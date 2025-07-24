fd0af712a05beab610cfa11be5dba7ed32d055a4
Michelle Abt <mabt924@gmail.com>
2025-07-24 12:55:30 -0700

    Initial Commit

---
+++ BirthdaysTests/BirthdaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysTests.swift
+//  BirthdaysTests
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import Testing
+@testable import Birthdays
+
+struct BirthdaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

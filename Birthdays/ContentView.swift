2679fd5981bf708dfb5156b9ae6fe06d8a0c8ef3
Michelle Abt <mabt924@gmail.com>
2025-07-24 13:25:58 -0700

    Interation 0 done

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [Friend(name: "Tracey B", birthday: .now), Friend(name: "Aditi", birthday: Date(timeIntervalSince1970: 0)), Friend(name: "Eliana", birthday: Date(timeIntervalSince1970: 15))]
+    
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }//hstack
+                
+            }//list
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
+        }//nav stack
+        
+    }//body
+}//struct
 
 #Preview {
     ContentView()
